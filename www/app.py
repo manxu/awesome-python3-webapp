3728d8d8ba8da9a292e74308bd0c64512c6cae73
zhangc <1142204485@qq.com>
2018-10-23 07:33:10 +0000

    autocommit

--- www/app.py
+++ www/app.py
@@ -1,90 +1,21 @@
-import flask
-from flask import request
-from www.sqlconfig import Ip, app, AIServer,scheduler,db
-from www.serverReq import gethttp
-import numpy as np
-import datetime
-from sshserver.config import paramikoclient
-from sshserver.test1 import transportclient
+import logging; logging.basicConfig(level=logging.INFO)
 
-client={}
+import asyncio, os, json, time
+from datetime import datetime
 
-@app.route('/',methods = ['GET','POST'])
-def index():
-    if request.method == 'POST':
-        filters = []
-        if request.values.get('ip')!='':
-            filters.append(AIServer.ip==request.values.get('ip'))
-        if request.values.get('port')!='':
-            filters.append(AIServer.port == request.values.get('port'))
-        if request.values.get('server_type')!='':
-            filters.append(AIServer.server_type == request.values.get('server_type'))
-        if request.values.get('server_addr')!='':
-            filters.append(AIServer.server_addr == request.values.get('server_addr'))
-        f = np.array(filters)
-        servers = AIServer.query.filter(*f).order_by("server_status").all()
-    else:
-        servers = AIServer.query.order_by("server_status").all()
-    return flask.render_template('server.html',servers=servers,form = request.form)
+from aiohttp import web
 
-@app.route('/addServer',methods=['POST'])
-def addServer():
-    form = request.form
-    a = AIServer('1',form['ip'],form['port'],form['server_addr'],form['server_type'],form['server_remark'],0,datetime.datetime.now(),'sys')
-    db.session.add(a)
-    db.session.commit()
-    return 'dataFromAjax'
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
 
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...') 
+    return srv
 
-# cmd
-@app.route('/cmd',methods=['GET'])
-def cmd():
-    arg = request.args['arg']
-    client_cmd = paramikoclient()
-    alt = client_cmd.run_ssh(arg)
-    client_cmd.close()
-    return  alt
-# 连接linux
-@app.route('/connect1',methods=['GET'])
-def connect1():
-    global client
-    shell = client.get('172.17.34.4')
-    if shell==None:
-        shell = transportclient('172.17.34.4',22, 'root', 'dev_201704')
-        client['172.17.34.4'] = shell
-    x,y = shell.connect()
-    if not x:
-        return '连接失败'
-    return y
-
-# 连接linux
-@app.route('/close',methods=['GET'])
-def close():
-    global client
-    shell = client.get('172.17.34.4')
-    shell.close()
-    return 'success'
-
-# shell
-@app.route('/shell',methods=['GET'])
-def shell():
-    arg = request.args['arg']
-    global client
-    shell = client.get('172.17.34.4')
-    if shell==None:
-        shell = transportclient('172.17.34.4',22, 'root', 'dev_201704')
-        if not shell.connect():
-            return '连接失败'
-        client['172.17.34.4'] = shell
-    alt = shell.send(arg)
-    # shell.close()
-    return  alt
-
-
-
-if __name__ == '__main__':
-    app.debug=True
-    scheduler.add_job(func=gethttp, id='1', args=(), trigger='interval', seconds=60, replace_existing=False)
-    scheduler.init_app(app=app)
-    scheduler.start()
-    app.run()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
